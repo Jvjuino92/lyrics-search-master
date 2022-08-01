25b8a45751bb154fb2a125841150c449697d009e
Jvjuino92 <jvmmutiz@gmail.com>
2022-08-01 11:25:39 +0100

    2nd commit, project is slow but still working

---
+++ app.js
@@ -0,0 +1,44 @@
+const form = document.querySelector('#form')
+const searchInput = document.querySelector('#search')
+const songsContainer = document.querySelector('#songs-container')
+const prevAndNextContainer = document.querySelector('#prev-and-next-container')
+
+const apiURL = `https://api.lyrics.ovh`
+
+const insertSongsIntoPage = songsInfo => {
+    songsContainer.innerHTML = songsInfo.data.map(song => 
+        `<li class="song">
+            <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+            <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">See lyrics</button>
+        </li>`
+    ).join('')
+
+    if (songsInfo.prev || songsInfo.next) {
+        prevAndNextContainer.innerHTML = `
+            ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Next</button>` : ''}
+        `
+    }
+
+}
+
+const fetchSongs = async term => {
+    const response = await fetch(`${apiURL}/suggest/${term}`)
+    const data = await response.json()
+
+    insertSongsIntoPage(data)
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const searchTerm = searchInput.value.trim()
+
+    if (!searchTerm) {
+        songsContainer.innerHTML = `<li class="warning-message">Please insert a valid song or artist</li>`
+        return
+    }
+
+    fetchSongs(searchTerm)
+})
+
+// 40:55
