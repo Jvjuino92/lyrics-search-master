bbd8492e0a39c9e6316d120103aa33d645404ae1
Jvjuino92 <jvmmutiz@gmail.com>
2022-08-02 10:45:46 +0100

    next button is not working, error 429 too many req

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const apiURL = `https://api.lyrics.ovh`
 
+const getMoreSongs = async url => {
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`   )
+    const data = await response.json()
+
+    insertSongsIntoPage(data)
+}
+
 const insertSongsIntoPage = songsInfo => {
     songsContainer.innerHTML = songsInfo.data.map(song => 
         `<li class="song">
@@ -15,10 +22,13 @@ const insertSongsIntoPage = songsInfo => {
 
     if (songsInfo.prev || songsInfo.next) {
         prevAndNextContainer.innerHTML = `
+            ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Previous</button>` : ''}
             ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Next</button>` : ''}
         `
+        return
     }
 
+    prevAndNextContainer.innerHTML = ''
 }
 
 const fetchSongs = async term => {
@@ -41,4 +51,15 @@ form.addEventListener('submit', event => {
     fetchSongs(searchTerm)
 })
 
-// 40:55
+songsContainer.addEventListener('click', event => {
+    const clickedElement = event.target
+
+    if (clickedElement.tagName === 'BUTTON') {
+        const artist = clickedElement.getAttribute('data-artist')
+        const songTitle = clickedElement.getAttribute('data-song-title')
+
+        console.log({ artist, songTitle });
+    }
+})
+
+// 52:18
